2d51c3aad2acbf05dcab88c7ec1ec84e911720b4
Dawid Kruk <dawid.kruk@ovoo.pl>
2024-04-24 17:27:13 +0200

    Add send, create vesting account and send to vesting account messages

--- src/components/SplitVesting.vue
+++ src/components/SplitVesting.vue
@@ -2,13 +2,17 @@
 import {ref} from 'vue'
 import {AminoTypes, SigningStargateClient} from "@cosmjs/stargate";
 import {coins, DirectSecp256k1HdWallet} from "@cosmjs/proto-signing";
-import {createKeplrConfig, createVestingAminoConverters, getFees} from "./helpers";
+import {createVestingAminoConverters, getFees} from "./helpers";
 import {
-  MsgMoveAvailableVesting,
+  MsgCreateVestingAccount,
+  MsgMoveAvailableVesting, MsgSendToVestingAccount,
   MsgSplitVesting
 } from "../../ts-client/chain4energy.c4echain.cfevesting/types/c4echain/cfevesting/tx";
 import {registry} from "../../ts-client";
 import {blockchainConfig} from "../blockchainConfig";
+import {TxRaw} from "../../ts-client/cosmos.tx.v1beta1";
+import {MsgSend} from "../../ts-client/cosmos.bank.v1beta1/types/cosmos/bank/v1beta1/tx";
+import {Keplr} from "@keplr-wallet/types";
 
 enum OperationType {
   split = "split",
@@ -24,37 +28,45 @@ const loader = ref(false)
 const toAddress = ref("")
 const operation = ref()
 const fromAddress = ref("")
+const createVestingAccountStarttime = ref("")
+const createVestingAccountEndtime = ref("")
+const vestingPoolName = ref("")
+const restartVesting = ref(false)
 
-let client:SigningStargateClient
+let client: SigningStargateClient
 
-declare global { interface Window {keplr:any} }
+declare global {
+  interface Window {
+    keplr: Keplr;
+  }
+}
 
 const connectWithKeplr = async () => {
-    // const chainInfo = createKeplrConfig();
-    // await window.keplr.experimentalSuggestChain(chainInfo);
-    // await window.keplr.enable(chainInfo.chainId);
-    // const offlineSigner = window.keplr.getOfflineSignerOnlyAmino(chainInfo.chainId);
-   const offlineSigner = await DirectSecp256k1HdWallet.fromMnemonic("cabin gun luggage green fever either engage nest arrow sure record physical palace dutch clap vocal north snack birth exhibit either ancient sniff settle", {prefix: "c4e"})
-    const accounts = await offlineSigner.getAccounts();
-    userAddress.value = accounts[0].address
-    const aminoTypes = new AminoTypes(createVestingAminoConverters())
-    client = await SigningStargateClient.connectWithSigner(
-        blockchainConfig.rpcUrl,
-        offlineSigner,
-        {registry, aminoTypes}
-    );
+  // const chainInfo = createKeplrConfig();
+  // await window.keplr.experimentalSuggestChain(chainInfo);
+  // await window.keplr.enable(chainInfo.chainId);
+  // const offlineSigner = window.keplr.getOfflineSignerOnlyAmino(chainInfo.chainId);
+  const offlineSigner = await DirectSecp256k1HdWallet.fromMnemonic("cabin gun luggage green fever either engage nest arrow sure record physical palace dutch clap vocal north snack birth exhibit either ancient sniff settle", {prefix: "c4e"})
+  const accounts = await offlineSigner.getAccounts();
+  userAddress.value = accounts[0].address
+  const aminoTypes = new AminoTypes(createVestingAminoConverters())
+  client = await SigningStargateClient.connectWithSigner(
+      blockchainConfig.rpcUrl,
+      offlineSigner,
+      {registry, aminoTypes}
+  );
 }
 connectWithKeplr()
 
-const splitVesting = async() => {
+const splitVesting = async () => {
   if (confirm("Confirm transaction")) {
     loader.value = true
     try {
-      const coinsToSend = new coins(amount.value, "uc4e");
+      const coinsToSend = coins(amount.value, "uc4e");
       const msgSplitVestsing: MsgSplitVesting = {
-          amount: coinsToSend,
-          fromAddress: fromAddress.value,
-          toAddress: toAddress.value,
+        amount: coinsToSend,
+        fromAddress: fromAddress.value,
+        toAddress: toAddress.value,
       }
       await signAndBroadcast(createEncodedMsgSplitVesting(msgSplitVestsing))
     } catch (e) {
@@ -65,13 +77,71 @@ const splitVesting = async() => {
   }
 }
 
-const moveAvailableVesting = async() => {
+const sendToVestingAccount = async () => {
   if (confirm("Confirm transaction")) {
     loader.value = true
     try {
-      const msgMoveAvailableVesting : MsgMoveAvailableVesting= {
-          fromAddress: fromAddress.value,
-          toAddress: toAddress.value,
+      const msgSendToVestingAccount: MsgSendToVestingAccount = {
+        owner: fromAddress.value,
+        toAddress: toAddress.value,
+        amount: amount.value + "",
+        vestingPoolName: vestingPoolName.value,
+        restartVesting: restartVesting.value,
+      }
+      console.log(msgSendToVestingAccount)
+      await signAndBroadcast(createEncodedMsgSendToVestingAccount(msgSendToVestingAccount))
+    } catch (e) {
+      loader.value = false
+      alert("Error: " + e.toString())
+    }
+  }
+}
+
+const sendCoins = async () => {
+  if (confirm("Confirm transaction")) {
+    loader.value = true
+    try {
+      const coinsToSend = coins(amount.value, "uc4e");
+      const msgSend: MsgSend = {
+        fromAddress: fromAddress.value,
+        toAddress: toAddress.value,
+        amount: coinsToSend,
+      }
+      await signAndBroadcast(createEncodedMsgSend(msgSend))
+    } catch (e) {
+      loader.value = false
+      alert("Error: " + e.toString())
+    }
+  }
+}
+
+const createVestingAccount = async () => {
+  if (confirm("Confirm transaction")) {
+    loader.value = true
+    try {
+      const coinsToSend = coins(amount.value, "uc4e");
+      const msgCreateVestingAccount: MsgCreateVestingAccount = {
+        fromAddress: fromAddress.value,
+        toAddress: toAddress.value,
+        startTime: +createVestingAccountStarttime.value,
+        endTime: +createVestingAccountEndtime.value,
+        amount: coinsToSend,
+      }
+      await signAndBroadcast(createEncodedMsgCreateVestingAccount(msgCreateVestingAccount))
+    } catch (e) {
+      loader.value = false
+      alert("Error: " + e.toString())
+    }
+  }
+}
+
+const moveAvailableVesting = async () => {
+  if (confirm("Confirm transaction")) {
+    loader.value = true
+    try {
+      const msgMoveAvailableVesting: MsgMoveAvailableVesting = {
+        fromAddress: fromAddress.value,
+        toAddress: toAddress.value,
       }
       await signAndBroadcast(createEncodedMsgMoveAvailableVesting(msgMoveAvailableVesting))
     } catch (e) {
@@ -82,10 +152,16 @@ const moveAvailableVesting = async() => {
 }
 
 
-
-const signAndBroadcast =async  (encoded: any) => {
+const signAndBroadcast = async (encoded: any) => {
   try {
-    const res = await client.signAndBroadcast(userAddress.value, [encoded], getFees(), "ABC=")
+    const {accountNumber, sequence} = await client.getSequence(fromAddress.value);
+    const signed = await client.sign(userAddress.value, [encoded], getFees(), "ABC=", {
+      sequence,
+      accountNumber,
+      chainId: blockchainConfig.chainId,
+    })
+    const txBytes = TxRaw.encode(signed).finish();
+    const res = await client.broadcastTx(txBytes, 10000, 10000);
     if (res.code === 0) {
       loader.value = false
       alert("Transaction successful!")
@@ -113,10 +189,32 @@ const createEncodedMsgMoveAvailableVesting = (objectToEncode: any) => {
   };
 }
 
+const createEncodedMsgSend = (objectToEncode: any) => {
+  return {
+    typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+    value: objectToEncode,
+  };
+}
+
+const createEncodedMsgCreateVestingAccount = (objectToEncode: any) => {
+  return {
+    typeUrl: "/chain4energy.c4echain.cfevesting.MsgCreateVestingAccount",
+    value: objectToEncode,
+  };
+}
+
+const createEncodedMsgSendToVestingAccount = (objectToEncode: any) => {
+  return {
+    typeUrl: "/chain4energy.c4echain.cfevesting.MsgSendToVestingAccount",
+    value: objectToEncode,
+  };
+}
+
 const chooseOperation = (op: OperationType) => {
   operation.value = op
   toAddress.value = ""
   amount.value = ""
+  fromAddress.value = ""
 }
 
 </script>
@@ -125,44 +223,82 @@ const chooseOperation = (op: OperationType) => {
   <div id="wrapper">
     <div v-if="!loader">
       <div class="wrapper">
-          <div v-if="userAddress">
-              <h3>User address: {{userAddress}}</h3>
-          </div>
-        <div v-if="!userAddress" >
-            <h2>To split vesting connect with keplr first</h2>
-            <button @click="connectWithKeplr()">Connect with keplr</button>
-<!--            TODO: add if needed-->
-<!--            <hr style="border: 1px solid grey; width: 100%"/>-->
-<!--            <h3>Enter mnemonic:</h3>-->
-<!--            <textarea v-model="mnemonic"></textarea>-->
-<!--            <button @click="retrieveKey(); retrieved = true">Connect with mnemonic</button>-->
+        <div v-if="userAddress">
+          <h3>User address: {{ userAddress }}</h3>
+        </div>
+        <div v-if="!userAddress">
+          <h2>To split vesting connect with keplr first</h2>
+          <button @click="connectWithKeplr()">Connect with keplr</button>
+          <!--            TODO: add if needed-->
+          <!--            <hr style="border: 1px solid grey; width: 100%"/>-->
+          <!--            <h3>Enter mnemonic:</h3>-->
+          <!--            <textarea v-model="mnemonic"></textarea>-->
+          <!--            <button @click="retrieveKey(); retrieved = true">Connect with mnemonic</button>-->
         </div>
 
         <div v-if="userAddress">
-          <div>
+          <div style="display: flex; gap: 10px">
             <h3>Choose operation</h3>
-            <button style="margin-right: 5px" @click="chooseOperation(OperationType.split)">Split vesting</button>
+            <button @click="chooseOperation(OperationType.split)">Split vesting</button>
             <button @click="chooseOperation(OperationType.move)">Move available vesting</button>
+            <button @click="chooseOperation(OperationType.send)">Send coins</button>
+            <button @click="chooseOperation(OperationType.createVestingAccount)">Create vesting account</button>
+            <button @click="chooseOperation(OperationType.sendToVestingAccount)">Send coins to vesting account</button>
           </div>
           <hr style="border: 1px solid grey; width: 100%" v-if="operation"/>
           <div class="centeredDiv" v-if="operation === OperationType.move">
             <h3>Account from</h3>
-            <input v-model="fromAddress" type="text" />
-            <h3>Move available vesting to new account: </h3>
-            <input v-model="toAddress" type="text" />
+            <input v-model="fromAddress" type="text"/>
+            <h3>To address: </h3>
+            <input v-model="toAddress" type="text"/>
             <button @click="moveAvailableVesting()">Move available vesting</button>
           </div>
           <div class="centeredDiv" v-if="operation === OperationType.split">
             <h3>Account from</h3>
-            <input v-model="fromAddress" type="text" />
-            <h3>Split vesting to new vesting acount: </h3>
-            <input v-model="toAddress" type="text" />
+            <input v-model="fromAddress" type="text"/>
+            <h3>To address: </h3>
+            <input v-model="toAddress" type="text"/>
             <h3>Amount to split (uc4e): </h3>
-            <input v-model="amount" type="number" />
+            <input v-model="amount" type="number"/>
             <button @click="splitVesting()">Split vesting</button>
           </div>
+          <div class="centeredDiv" v-if="operation === OperationType.send">
+            <h3>Account from</h3>
+            <input v-model="fromAddress" type="text"/>
+            <h3>Send coins to: </h3>
+            <input v-model="toAddress" type="text"/>
+            <h3>Amount to send (uc4e): </h3>
+            <input v-model="amount" type="number"/>
+            <button @click="sendCoins()">Send coins</button>
         </div>
+          <div class="centeredDiv" v-if="operation === OperationType.createVestingAccount">
+            <h3>Account from</h3>
+            <input v-model="fromAddress" type="text"/>
+            <h3>To address: </h3>
+            <input v-model="toAddress" type="text"/>
+            <h3>Amount to send (uc4e): </h3>
+            <input v-model="amount" type="number"/>
+            <h3>Start time: </h3>
+            <input v-model="createVestingAccountStarttime" type="number"/>
+            <h3>End time: </h3>
+            <input v-model="createVestingAccountEndtime" type="number"/>
+            <button @click="createVestingAccount()">Create vesting account</button>
+          </div>
+          <div class="centeredDiv" v-if="operation === OperationType.sendToVestingAccount">
+            <h3>Account from</h3>
+            <input v-model="fromAddress" type="text"/>
+            <h3>To address: </h3>
+            <input v-model="toAddress" type="text"/>
+            <h3>Amount to send (uc4e): </h3>
+            <input v-model="amount" type="number"/>
+            <h3>Vesting pool name:</h3>
+            <input v-model="vestingPoolName" type="text"/>
+            <h3>Restart vesting</h3>
+            <input v-model="restartVesting" type="checkbox"/>
+            <button @click="sendToVestingAccount()">Send coins to vesting account</button>
+          </div>
       </div>
+    </div>
     </div>
 
     <div v-if="loader">
@@ -181,6 +317,7 @@ const chooseOperation = (op: OperationType) => {
   padding-top: 40px;
   background: orange;
 }
+
 textarea {
   min-height: 150px;
   border-radius: 10px;
@@ -189,6 +326,7 @@ textarea {
   font-size: 15px;
   box-shadow: 3px 4px 4px grey;
 }
+
 input {
   border-radius: 10px;
   border: 1px solid;
@@ -196,6 +334,7 @@ input {
   font-size: 15px;
   box-shadow: 3px 4px 4px grey;
 }
+
 .wrapper {
   display: flex;
   flex-direction: column;
@@ -213,15 +352,15 @@ input {
 }
 
 .wrapper > div {
-    box-shadow: 2px 5px 24px 0px rgba(66, 68, 90, 1);
-    margin-top: 10px;
-    padding: 10px;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 600px;
-    background: white;
+  box-shadow: 2px 5px 24px 0px rgba(66, 68, 90, 1);
+  margin-top: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 600px;
+  background: white;
 }
 
 </style>
